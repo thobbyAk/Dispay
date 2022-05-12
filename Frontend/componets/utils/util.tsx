2e558409bf389fbca3189140fe130cc332d398ae
Oluwatobi Akinrinade <tobi@dystopialabs.com>
2022-05-12 16:17:08 +0100

    group setup

--- Frontend/componets/utils/util.tsx
+++
@@ -1,9 +0,0 @@
-export const shortenAddress = (address: string) => {
-    if (!address) return "";
-    if (address.startsWith("0x")) {
-      if (address.length < 12) return address;
-      return address.slice(0, 6) + "..." + address.slice(-4);
-    }
-    if (address.length < 10) return address;
-    return address.slice(0, 4) + "..." + address.slice(-4);
-  };
